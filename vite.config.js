68c60f6498fe6b6b0cf554333e1973d1bcb16f8b
Mridul Negi <negimridul2005@gmail.com>
2025-08-27 22:59:24 +0530

    Added PWA functionality

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'The Loop',
+        short_name: 'Loop',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#0d1117',
+        theme_color: '#7c3aed',
+        description: "Never miss out on what's happening on campus. The Loop is your one-stop shop for all college events.",
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
